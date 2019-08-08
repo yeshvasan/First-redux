be65f5b0943eb9308324b84f6531059e8219718e
yeshvasan <48148176+yeshvasan@users.noreply.github.com>
2019-08-08 19:05:42 +0530

    Update Post.js

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
-import {FaTrash, FaEdit} from 'react-icons/fa';
+import {FaTrash, FaEdit,FaBrush} from 'react-icons/fa';
 
 class Post extends Component {
     render() {
         return (
             <div>
 <h2>{this.props.post.title}</h2>
-<p>{this.props.post.message}</p>
-<div className="d-flex justify-content-between">
+<div className="d-flex justify-content-around">
 <button className="btn btn-warning but" onClick={()=>this.props.dispatch({type:'EDIT_POST', id:this.props.post.id})}><FaEdit /></button>
 <button className="btn btn-danger but"  onClick={()=>this.props.dispatch({type:'DELETE_POST',id:this.props.post.id})}><FaTrash /></button>
+<button className="btn btn-danger but"  onClick={()=>this.props.dispatch({type:'STATUS',id:this.props.post.id})}><FaBrush /></button>
 </div>
             </div>
         )
